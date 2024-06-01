fd5e5cf9e170470e11d06fa08f38da7aa55f8a73
vincentrandla <vincentrandla@gmail.com>
2024-06-01 22:58:27 +0300

    Added qr scanner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+    implementation ("com.google.zxing:core:3.3.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/siili/MainActivity.kt
+++ app/src/main/java/com/example/siili/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.example.siili
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
-
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mealTextView: TextView
     private lateinit var scanButton: Button
-    private var sentence: String = "Sul on 7 päevapraadi tasuta praeni!"
+    private var sentence: String = "Sul on 7 päeva tasuta praeni!"
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mealTextView = findViewById(R.id.mealTextView)
         scanButton = findViewById(R.id.scanButton)
-
         mealTextView.text = sentence
 
         scanButton.setOnClickListener {
-            sentence = StringUtil.countdownNumberInSentence(sentence)
-            mealTextView.text = sentence
+            val integrator = IntentIntegrator(this@MainActivity)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            integrator.setPrompt("Scan a QR code")
+            integrator.setBeepEnabled(true)
+            integrator.setBarcodeImageEnabled(true)
+            integrator.initiateScan()
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        result?.let {
+            if (it.contents != null) {
+                // Handle the QR code content here
+                // For this example, we'll assume the QR code scan always means subtracting 1
+                sentence = StringUtil.countdownNumberInSentence(sentence)
+                mealTextView.text = sentence
+            }
+        } ?: super.onActivityResult(requestCode, resultCode, data)
+    }
 }
+
